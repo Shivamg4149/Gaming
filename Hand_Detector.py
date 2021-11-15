8a6e2ba6adb837a6e53a27605d519b39a063ecec
Shivamg4149 <53791432+Shivamg4149@users.noreply.github.com>
2021-11-15 09:09:36 +0530

    Add files via upload

---
+++ Hand_Detector.py
@@ -0,0 +1,44 @@
+import cv2
+import mediapipe as mp
+import os
+class handDetector():
+    def __init__(self,mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+    def findPosition(self, img, handNo=0, draw=True):
+
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+
+                h,w,c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+                lmList.append([id, cx, cy])
+                #print(lmList)
+                if draw:
+                    cv2.circle(img, (cx,cy), 15, (255,0,255), cv2.FILLED)
+        return lmList
+
+def HandDetection(img,draw):
+    detector = handDetector(detectionCon=0.75)
+    img = detector.findHands(img,draw)
+    myList = detector.findPosition(img,draw=False)
+    return img,myList
+        
